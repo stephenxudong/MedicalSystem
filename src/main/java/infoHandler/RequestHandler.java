da177e67f9156b35382099ffd15294e250117c11
侯云 <l>
2018-05-14 19:36:22 +0800

    123
    更改过后的socket

--- src/main/java/infoHandler/RequestHandler.java
+++ src/main/java/infoHandler/RequestHandler.java
@@ -7,26 +7,28 @@ import java.io.IOException;
 
 public class RequestHandler implements Runnable{
     private BufferedWriter writer;
-    private String requestStr;
+    private String[] requestArray;
+    private int doctorID;
     private boolean loginState;
 
-    public RequestHandler(String requestStr, BufferedWriter writer, boolean loginState)
+    public RequestHandler(String[] requestArray, BufferedWriter writer, boolean loginState, int doctorID)
     {
-        this.requestStr = requestStr;
+        this.requestArray = requestArray;
         this.writer = writer;
         this.loginState = loginState;
+        this.doctorID = doctorID;
     }
 
     @Override
     public void run() {
-        String decodeStr = AESEncode.aesDecrypt(requestStr);
-        String[] requestArray = decodeStr.split("#"); //获得切分出来的数组
         String returnMess = matchRes(requestArray[0], getValues(requestArray)); //取得有效值部分,执行返回操作
         returnMess = AESEncode.aesDecrypt(returnMess); //加密
         try {
             writer.write(returnMess);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            Thread.currentThread().interrupt();
         }
     }
 
@@ -46,9 +48,6 @@ public class RequestHandler implements Runnable{
 
         String rentunMess = "";
         switch (requestMess) {
-            case "$LOGIN$":
-                rentunMess = ResponseEnum.$LOGIN_STATE$.toString() + loginResponse(usefulValues);
-                break;
             case "$UPDATE$":
                 rentunMess = ResponseEnum.$UPDATE_STATE$.toString() + updateResponse(usefulValues);
                 break;
@@ -58,21 +57,11 @@ public class RequestHandler implements Runnable{
             case "$REFRESH_CONTENT$":
                 rentunMess = ResponseEnum.$RES_CONTENT$.toString() + refreshContent(usefulValues);
                 break;
-
+            case "$RECEIVE_STATE$":
+                refreshReportStat(usefulValues);
+                break;
         }
         return rentunMess;
-    }
-
-    public String loginResponse(String[] usefulValues)
-    { //usefulvalues[0] = 用户名, usefualValues[1] = 密码;
-        boolean loginStat = false;
-
-        //todo 查数据库, 如果可以登录, 将其值改为true，
-        //todo 随便改，最后返回字符串，成功为true，失败为false
-
-        if (loginStat)
-            return "true";
-        return "false";
     }
 
     public String updateResponse(String[] usefulValues)
@@ -89,7 +78,7 @@ public class RequestHandler implements Runnable{
         if (!loginState)
             return "FuckYou，hacker";
 
-        //todo 函数目的：响应手动刷新目录的请求
+        //todo 函数目的：响应手动刷新目录的请求 把doctorid对应的病历号全弄出来
         //todo 返回该医生对应的所有病例单号，放入json数组中，eg [2016302580225,201630258223,2016302580224,...]
         //todo 把json对象封装为string
         return "";
@@ -100,9 +89,15 @@ public class RequestHandler implements Runnable{
         if (!loginState)
             return "FuckYou，hacker";
 
-        //todo 函数目的：响应手动刷新病历的请求
+        //todo 函数目的：响应手动刷新病历的请求，把参数中的病历号对应的病历返回
         //todo 返回客户端索要的病历内容
         //todo 返回内容需要封装为string
         return "";
+    }
+
+    public void refreshReportStat(String[] usefulValues)
+    {
+        //todo usefulValues存放客户端新接收的病历内容（对应refreshContent）的病历单号，
+        //todo 接到后就把数据库中对应的病历状态设置为已查看
     }
 }
--- src/main/java/socket/ThreadSocket.java
+++ src/main/java/socket/ThreadSocket.java
@@ -3,6 +3,7 @@ package socket;
 import ThreadPool.SocketPool;
 import infoHandler.RequestHandler;
 import infoHandler.ResponseEnum;
+import locker.AESEncode;
 
 import java.io.*;
 import java.net.Socket;
@@ -10,12 +11,12 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 class ThreadSocket implements Runnable {
 
+    private ThreadPoolExecutor threadPoolExecutor;
     private Socket threadSocket;
     private BufferedReader reader;
     private BufferedWriter writer;
     private boolean loginState;
-    private ThreadPoolExecutor threadPoolExecutor;
-
+    private int doctorID;
     private String heartBeatStr = "!@";
 
     public ThreadSocket(Socket socket) {
@@ -28,26 +29,39 @@ class ThreadSocket implements Runnable {
         try {
             reader = new BufferedReader(new InputStreamReader(threadSocket.getInputStream())); //从socket里面读入
             writer = new BufferedWriter(new OutputStreamWriter(threadSocket.getOutputStream()));
-            char[] buf = new char[4096];  int len;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int len;
+        char[] buf = new char[4096];
+        try {
             while(threadSocket.isConnected()){ //后又阻塞在这等待客户端发消息
                 if ((len = reader.read(buf)) != -1) { //接收到了数据
                     String receivedStr = new String(buf, 0, len);
-                    if (receivedStr.equals(heartBeatStr)); //接受心跳包
+                    String decodeStr = AESEncode.aesDecrypt(receivedStr); //获得解密的请求数据
+                    String[] requestArray = decodeStr.split("#"); //获得切分出来的数组
+                    if (decodeStr.equals(heartBeatStr)); //接受心跳包
+                    else if(requestArray[0].equals("$LOGIN$")) //登录
+                    {
+                        doctorID = Integer.parseInt(requestArray[1]);
+                        writer.write(ResponseEnum.$LOGIN_STATE$.toString() + loginResponse(requestArray));
+                    }
                     else { //数据处理线程，减少try catch块中代码量
                         threadPoolExecutor = new SocketPool().getRequestHandlerPool();
-                        threadPoolExecutor.execute(new Thread(new RequestHandler(receivedStr, writer, loginState)));
+                        threadPoolExecutor.execute(new Thread(new RequestHandler(requestArray, writer, loginState, doctorID)));
                     }
                 }
                 String appoinFlushState = checkAppState(); //检查有无没有被下载的病历信息
                 if(!(appoinFlushState.equals("failed"))){
-                    writer.write(ResponseEnum.$NEW_RECORD_REMIND$.toString() + appoinFlushState);
-                    writer.flush();
-                }
+                     if (writer == null)
+                        writer.write(ResponseEnum.$NEW_RECORD_REMIND$.toString() + appoinFlushState);
+                     writer.flush();
+                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-           disconnect(); //关闭流,socket和线程
+            disconnect(); //关闭流,socket和线程
         }
     }
 
@@ -61,6 +75,18 @@ class ThreadSocket implements Runnable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public String loginResponse(String[] usefulValues)
+    {
+        String returnStr = "false";
+        //todo usefulvalues[1] = 用户名, usefualValues[2] = 密码;
+        //todo 查数据库, 如果可以登录, 将其值改为true，
+        //todo 随便改，最后返回字符串，成功为true，失败为false
+        //todo 更改loginstate的值
+        if (loginState)
+            returnStr = "true";
+        return returnStr;
     }
 
     public String checkAppState() {
