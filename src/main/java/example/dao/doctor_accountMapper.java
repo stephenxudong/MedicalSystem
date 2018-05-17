08bc5dab431a587e035641b4dea86a10119e45f7
侯云 <l>
2018-05-17 08:43:11 +0800

    12
    1321

--- src/main/java/example/dao/doctor_accountMapper.java
+++ src/main/java/example/dao/doctor_accountMapper.java
@@ -4,5 +4,5 @@ import example.pojo.doctor_account;
 import org.apache.ibatis.annotations.Param;
 
 public interface doctor_accountMapper {
-    doctor_account loginResponse(@Param(value="userName")String name, @Param(value="userPassword")String area);
+    doctor_account loginResponse(@Param(value="doctor_account_id")String doctor_account_id, @Param(value="userPassword")String area);
 }
